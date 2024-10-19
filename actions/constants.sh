c25e95661c3c905a1443f1f5a6c59a6d947031da
Thomas Lindop <thomas16256@gmail.com>
2024-10-19 20:52:35 +0100

    >> restore changing "workspace" by number

--- actions/constants.sh
+++ actions/constants.sh
@@ -1,3 +1,3 @@
-STATE_DIR=$XDG_RUNTIME_DIR/river
-CURRENT_WS_FILE=$STATE_DIR/current_workspace
-CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/river
+export STATE_DIR=$XDG_RUNTIME_DIR/river
+export CURRENT_WS_FILE=$STATE_DIR/current_workspace
+export CONFIG_DIR=${XDG_CONFIG_HOME:-$HOME/.config}/river
